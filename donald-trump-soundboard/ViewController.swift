3cae4f66014d60736d42c7438a004fcef40e8f59
Jimmy Brown <jabro4@gmail.com>
2016-01-14 15:03:09 -0800

    Buttons are not showing up in Preview but can still push them to play sound.

--- donald-trump-soundboard/ViewController.swift
+++ donald-trump-soundboard/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    let soundFileNames = ["america_great", "american_dream", "announcement", "beat_china", "beat_mexico", "big_lie", "blown_up", "bringing_drugs", "chevy_tokyo", "dying", "great_wall", "isis", "jobs_president", "laughing_border", "like_china", "losers", "need_leader", "need_money", "not_nice_person", "obama", "obamacare_website", "rapists", "really_rich", "stupid_people", "thank_you_darling"]
+    
+    var audioPlayers = [AVAudioPlayer]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Audio Players
+        for sound in soundFileNames {
+            
+            do {
+                
+                let url = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(sound, ofType: "mp3")!)
+                let audioPlayer = try AVAudioPlayer(contentsOfURL: url)
+                
+                audioPlayers.append(audioPlayer)
+                
+            } catch {
+                // Catch error
+                audioPlayers.append(AVAudioPlayer())
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +42,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonTapped(sender: BlueButton) {
+        
+        let audioPlayer = audioPlayers[sender.tag]
+        audioPlayer.play()
+    }
 
 }
 
